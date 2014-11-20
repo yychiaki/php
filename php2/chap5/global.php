f68ed3cee7efa6790ebd1a8626aeccf00ded3216
yychiaki <e14008@std.it-college.ac.jp>
2014-11-20 21:49:15 +0900

    141120-02

--- php2/chap5/global.php
+++ php2/chap5/global.php
@@ -2,13 +2,15 @@
 $dinner = 'Curry Cttlefish';
 
 function vegetarian_dinner(){
-	global $dinner;
-	print "Dinner was $dinner, but now it's";
-	$dinner = 'Sauteed Pea Shoots';
-	print $dinner;
+	//global $dinner;
+	print "Dinner was {$GLOBALS['dinner']}, but now it's";
+	$GLOBALS['dinner'] = 'Sauteed Pea Shoots';
+	print $GLOBALS['dinner'];
 	print "<br/>\n";
 }
 
 print "Regular Dinner is $dinner. <br/>\n";
 vegetarian_dinner();
 print "Regular dinner is $dinner";
+
+//$GLOBALSはファイルページをまたいで変数を参照する
